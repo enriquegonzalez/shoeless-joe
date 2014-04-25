af895a173dedf1436f01afc3956702477a734378
Enrique Gonalez <egonzalez78@gmail.com>
2014-04-25 16:01:46 -0500

    troubleshooting nav active class jquery

--- js/shoeless-joe.js
+++ js/shoeless-joe.js
@@ -1,11 +1,12 @@
 $(document).ready( function () {
 
 	$('window').on("load", "li a", function(){
+
 		var $menuItem = $(this).attr('href'),
 			url = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
 	   
-	   if( $menuItem === url) {
-	        $(this).addClass('active');
+	   if( $menuItem == url) {
+	        $menuItem.addClass('active');
 	   }
 	});
 
