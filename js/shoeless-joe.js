2bc951382f364db61cad3c711100c2ea73a37782
Enrique Gonzalez <egonzalez78@gmail.com>
2014-04-28 19:15:28 -0500

    nav menu active class behavior

--- js/shoeless-joe.js
+++ js/shoeless-joe.js
@@ -1,14 +1,10 @@
 $(document).ready( function () {
 
-  $('window').on("load", "li a", function(){
-
     $("li a").each(function () {
       var $menuItem = $(this),
           menuHref = $menuItem.attr('href');
 
        $("li a[href='" + window.location.href + "']").closest("li").addClass("active");
     });
-
-  });
 
 });
